10d50795c1c04b326d9c3dc749a76805d066eda5
bill <tuntunkimpo@naver.com>
2018-08-24 11:47:11 +0900

    change admin panle search, filter no css no html awesome

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from nomadgram.users import models as user_models
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,21 +11,36 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
+    
     """ image Model """
-
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
     
-
+    # representation
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+    
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
+    
     """ Comment Model """
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+
     image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
 
+    # representation
+    def __str__(self):
+        return self.message
 
 class Like(TimeStampedModel):
+    
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
     image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
